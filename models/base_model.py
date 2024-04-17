e671c0b6bdb66aa69a18573c027bfbd19b1217c4
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-04-17 14:16:09 +0300

    fix base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String
+import uuid
+from datetime import datetime
+from models import storage
 
 Base = declarative_base()
-
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -29,33 +27,24 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = type(self).__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        try:
-            del dictionary["_sa_instance_state"]
-        except KeyError:
-            pass
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
-        """
-        
-        """
-        from models import storage
+        """Deletes the current instance from storage"""
         storage.delete(self)
--- models/review.py
+++ models/review.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey
-from models.base_model import Base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ Review class to store review information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+
+class User(BaseModel, Base):
+    """ User class """
+    __tablename__ = 'users'
+    reviews = relationship('Review', cascade='all, delete', backref='user')
+
+
+class Place(BaseModel, Base):
+    """ Place class """
+    __tablename__ = 'places'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review', cascade='all, delete', backref='place')
+    else:
+        @property
+        def reviews(self):
+            """ Getter attribute for reviews """
+            reviews_list = []
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list

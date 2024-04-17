e0900504b591a82ddcea2a047a7cd21fade01afe
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-04-17 13:09:29 +0300

    fix

--- models/city.py
+++ models/city.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, String
+"""This represents the City class."""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
-    """
-    >>City inherits from BaseModel and Base (respect the order)
-    >>class attribute __tablename__ -
-            represents the table name, cities
-    >>class attribute name
-            represents a column containing a string (128 characters)
-            cant be null
-    >>class attribute state_id
-            represents a column containing a string (60 characters)
-            cant be null
-            is a foreign key to states.id
+    """This is the class definition for City.
+
+Attributes:
+- state_id: The state ID
+- name: Input name
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    state_id = Column(String(60),
+                      ForeignKey("states.id", ondelete="CASCADE"),
+                      nullable=False)
+    places = relationship(
+        "Place",
+        cascade="all, delete",
+        backref=backref("cities", cascade="all"),
+        passive_deletes=True)

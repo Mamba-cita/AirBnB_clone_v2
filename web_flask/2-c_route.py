e3ffe97ec744e7d98fd6b36ea1d15f02d1c77668
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-05-19 22:23:21 +0300

    initial

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
-
+"""This script starts the Flask web application"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello_hbnb():
+    """
+    Returns a string
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """
+    Returns a string
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_text(text):
+    """
+    Returns C followed by the value of the text variable
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    """
+    Ensures that the module is not executable when  imported
+    """
+    app.run(host='0.0.0.0', port=5000)
